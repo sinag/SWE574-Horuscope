8a8147181e3f8366c1be518f292c0cb2f0db6cf1
onebitme <ersuns@gmail.com>
2020-04-19 22:06:06 +0300

    Closes #58
    -Small Increment, changing the root/ and templates/base.html
    -Added a new app for #43

---
+++ appserver/userprofile/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User , AbstractUser
+from django.db import models
+
+
+class UserProfile():
+    user_name = "userName_dummy"
+    first_name = "Ersun?"
+    last_name = "Sozen?"
+    e_mail = "email@email.com"
