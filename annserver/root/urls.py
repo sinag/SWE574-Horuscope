2424676aaa8a9251bb520cc4b15a996019f5a923
Sina Gülşen <sinagulsen@gmail.com>
2020-04-27 18:21:14 +0300

    Closes #91

--- annserver/root/urls.py
+++ annserver/root/urls.py
@@ -16,15 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from template.views import TemplateViewSet
 from users import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'template', TemplateViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include('template.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
