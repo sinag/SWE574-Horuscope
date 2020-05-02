327248c8d159261ee3e0eca0ad7e9702cfb31040
onebitme <ersuns@gmail.com>
2020-05-02 18:46:42 +0300

    Userprofile app removed
    Userpage is now handled from user.

--- appserver/userprofile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CreateView
-from . import views
-
-app_name = 'userprofile'
-
-urlpatterns = [
-    path('profile/<int:userid>', CreateView.as_view(), name='profile_page'),
-]
