e0b325ebc400d649aadcf1657a2bcf02160b5a57
onebitme <ersuns@gmail.com>
2020-05-17 21:20:45 +0300

    Installations are complete.
    Created Manager.py as tutorial suggests
    Did updated settings.py
    added activity url

--- appserver/users/apps.py
+++ appserver/users/apps.py
@@ -1,4 +1,14 @@
 from django.apps import AppConfig
 
+
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        from actstream import registry
+        from comment.models import Comment
+        from community.models import Community
+        registry.register(self.get_model('CustomUser'))
+        registry.register(Comment)
+        registry.register(Community)
+
