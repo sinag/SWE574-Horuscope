0ee4478e8b2e33ac673529197f0e31a2f78bd61a
onebitme <ersuns@gmail.com>
2020-06-03 09:38:43 +0300

    Partially Closes #151
    * Default Profile Pic url from wiki when user created
    * I also left a check in html if we want static imaga as src
    * Solved 1-2-3-4-5

--- appserver/users/models.py
+++ appserver/users/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=30, blank=False)
     email = models.EmailField(max_length=254, blank=False)
     bio = models.CharField(max_length=500, blank=True)
-    profile_pic = models.URLField(max_length=2000, blank=True)
+    profile_pic = models.URLField(max_length=2000, blank=True, default="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png")
 
     def __str__(self):
         return self.email
