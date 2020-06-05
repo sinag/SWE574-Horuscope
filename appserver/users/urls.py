deb82bf67de042743c1c29aa2e1db61ecc374372
onebitme <ersuns@gmail.com>
2020-06-05 13:11:03 +0300

    Couldn't find how to redirect to Login
    @sinag it uses django's user auth,  when I encapsulate the ViewProfile w_ login_req() it directs to /accounts/login/../users/login

--- appserver/users/urls.py
+++ appserver/users/urls.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from .views import SignUpView, UpdateProfilePage, ViewProfilePage
 
 app_name = 'users'
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('view/<int:pk>/', ViewProfilePage.as_view(), name='view'),
+    path('view/<int:pk>/', login_required(ViewProfilePage.as_view()), name='view'),
     path('update/<int:pk>/', UpdateProfilePage.as_view(), name='update'),
 ]
--- appserver/users/views.py
+++ appserver/users/views.py
@@ -2,7 +2,6 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView, UpdateView, View
 from django.views import generic
 from .models import CustomUser
-
 from .forms import CustomUserCreationForm
 
 
@@ -20,7 +19,6 @@ class ViewProfilePage(generic.ListView):
     def get_queryset(self):
         return CustomUser.objects.filter(id=self.kwargs.get('pk')).first()
 
-
 class UpdateProfilePage(UpdateView):
     model = CustomUser
     fields = ['bio', 'profile_pic', ]
@@ -31,7 +29,4 @@ class UpdateProfilePage(UpdateView):
         return reverse('users:view', args=(user_id,))
 
     def get_queryset(self):
-        """
-        Get comment details to update
-        """
         return CustomUser.objects.filter(id=self.kwargs.get('pk'))
