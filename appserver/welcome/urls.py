30b876e8feb88c03b5819b17c2e8f2da75f14353
onebitme <ersuns@gmail.com>
2020-06-06 14:38:16 +0300

    Closes #180
    A welcome (landing) page

--- appserver/welcome/urls.py
+++ appserver/welcome/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from . import views
+
+app_name = 'welcome'
+urlpatterns = [
+    path('', views.home, name='welcome'),
+    path('home/', views.home, name='home'),
+]
--- appserver/welcome/views.py
+++ appserver/welcome/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+def home(request):
+    if request.user.is_authenticated:
+        return redirect('activitystream:index')
+    else:
+        return render(request, template_name='welcome/welcome.html')
