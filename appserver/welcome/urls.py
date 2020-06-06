8416a11db155c2214ab33ff39b658f5f5b85c6f0
onebitme <ersuns@gmail.com>
2020-06-06 19:26:25 +0300

    Minor Tweak to Header
    No issues, just connected the HoruScope button to about page

--- appserver/welcome/urls.py
+++ appserver/welcome/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'welcome'
 urlpatterns = [
     path('', views.home, name='welcome'),
+    path('about/', views.about, name='about'),
     path('home/', views.home, name='home'),
 ]
--- appserver/welcome/views.py
+++ appserver/welcome/views.py
@@ -2,8 +2,13 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+
 def home(request):
     if request.user.is_authenticated:
         return redirect('activitystream:index')
     else:
         return render(request, template_name='welcome/welcome.html')
+
+
+def about(request):
+    return render(request, template_name='welcome/welcome.html')
