df4d44823cc9a373a84f64f5a276b316597d5063
onebitme <ersuns@gmail.com>
2020-06-07 05:32:30 +0300

    Google PlacesAPI test

--- appserver/city/views.py
+++ appserver/city/views.py
@@ -9,22 +9,23 @@ def search_city(request):
     r_json = {}
     if request.POST:
         if request.POST['q']:
-            API_ENDPOINT = "https://api.geonames.org/searchJSON?"
+            API_ENDPOINT = "https://maps.googleapis.com/maps/api/place/autocomplete/json?"
             query = request.POST.get('q', False)
             print(query)
+            params = {
+                'input': query,
+                'types': "(cities)",
+                'key': "AIzaSyAftJH6eF1uB732blxSkmFyA2eYhA5nAPg"
+            }
+            geo_request = requests.get(API_ENDPOINT, params=params)
+            print(geo_request)
+            try:
+                data = geo_request.json()
+                print(data)
+            except ValueError:
+                print("Response content is not valid JSON")
+            return render(request, template_name='city/city_search.html')
+        else:
+            return HttpResponse("NOK")
 
-            # params = {
-            #     'q': query,
-            #     'maxRows':10,
-            #     'username':"demo"
-            # }
-            # geo_request = requests.get(API_ENDPOINT, params=params, verify=False)
-            # try:
-            #     data = geo_request.json()
-            # except ValueError:
-            #     print("Response content is not valid JSON")
-            g = geocoder.geonames(query, key='horuscope')
-            print(g.address)
-            return render(request, template_name='city/city_search.html', )
-        return HttpResponse("NOK")
-    return render(request, template_name='city/city_search.html')
+
