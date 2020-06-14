278e478b5162a96c0f03def3994d625bf916e347
Sina Gülşen <sinagulsen@gmail.com>
2020-06-15 01:04:53 +0300

    update init.py for annserver

--- annserver/initialize.py
+++ annserver/initialize.py
@@ -8,6 +8,6 @@ from annotations.models import Annotation
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'admin')
-    Annotation(owner_id=1, data="{\"@context\": \"http://www.w3.org/ns/anno.jsonld\",\"id\": "
-                                "\"http://example.org/anno1\",\"type\": \"Annotation\",\"body\": "
-                                "\"http://example.org/post1\",\"target\": \"http://example.com/page1\"}").save()
+    # Annotation(owner_id=1, data="{\"@context\": \"http://www.w3.org/ns/anno.jsonld\",\"id\": "
+    #                             "\"http://example.org/anno1\",\"type\": \"Annotation\",\"body\": "
+    #                             "\"http://example.org/post1\",\"target\": \"http://example.com/page1\"}").save()
